e3157f802aad6e95e224447e6198c8bddb20b47c
b-wild <bawilde79@gmail.com>
2023-01-16 14:27:41 -0500

    updated PyPoll code

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,24 +2,51 @@ import os
 import csv
 # importing the csv as read file
 Bank_df=os.path.join("resources","budget_data.csv")
-with open(Bank_df) as csvfile:
-    csvreader=csv.reader(csvfile, delimiter=",")
-    csv_header=next(csvreader)
+with open(Bank_df,'r') as csvfile:
+    Bank_info=csv.reader(csvfile, delimiter=",")
+    csv_header=next(Bank_info)
     print(f"CVS Header:{csv_header}")
-    for row in csvreader:
+    for row in Bank_info:
         #print(row)
-# determing the number of months
-    for months in range()
-        
-    print(f"There are (len(months)) months.")
-    
-    
+
+#The total number of months included in the dataset
+        month_total=()
+        for row in Bank_info:
+            months=row(0)
+            month_total.append(months)
+            print(len(month_total))
 
 
+#The net total amount of "Profit/Losses" over the entire period
+        #gainLosses =()
+        #for row in Bank_info:
+         #   gain_loses=(row(1+1)-row(1))
+          #  gainLosses.append(gain_loses)
+           # print(gainLosses)
+
+
+
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+        #change_profit =sum(gainLosses)
+            #print(f"Profit/Losses over entire period = {change_profit})
+        #percent_change=(change_profit/len(gainLosses)*100)
+            #print(f"Percent change over entire period = {percent_change})
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
 
 
 # Set variable for output file
-#output_file = os.path.join("banking_results.txt")
+print(f"""Financial Analysis
+  ----------------------------
+  Total Months: {months}
+  Total: {change_profit}
+  Average Change: {percent_change}
+  Greatest Increase in Profits: {greatest_increase}
+  Greatest Decrease in Profits: {greatest+decr3ease}""")
+  
+#output_file = os.path.join("banking_results.txt""")
 
 #  Open the output file
 #with open(output_file, "w") as datafile:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,51 @@
 import os
 import csv
+
+canidate_votes={}
+total_votes=0
+winner_vote=0
+winner_name=""
+
 # importing the csv as read file
 Election_df=os.path.join("resources","election_data.csv")
 with open(Election_df) as csvfile:
-    csvreader=csv.reader(csvfile, delimiter=",")
-    for row in csvreader:
+    Election_results=csv.reader(csvfile, delimiter=",")
+    csv_header=next(Election_results)
+    for row in Election_results:
         #print(row)
 
 # The total number of votes cast
-       
+        total_votes+=1
+        if row[2] in canidate_votes:
+            canidate_votes[row[2]]+=1
+        else:
+            canidate_votes[row[2]]=1
+
+#print(canidate_votes)  
+print(f"""Election Results
+--------------------------
+Total Votes: {total_votes}
+--------------------------""")          
+    
 # A complete list of candidates who received votes
+for canidate in canidate_votes:
+    print(f"{canidate} {canidate_votes[canidate]/total_votes *100} ( {canidate_votes[canidate]} )")
+    #print(f"Votes for canidate: {canidate_votes[canidate]}")
+    #print(f"percentage: {canidate_votes[canidate]/total_votes *100}")
+    #print("----------------------")
+    if winner_vote < canidate_votes[canidate]:
+        winner_vote=canidate_votes[canidate]
+        winner_name=canidate
 
-# The percentage of votes each candidate won
+print(f"""----------------------
+Name: {winner_name}
+-----------------""")
+ 
+#  Set variable for output file
+#output_file = os.path.join("Election_results.txt")
 
-# The total number of votes each candidate won
-
-# The winner of the election based on popular vote.
-
-# Set variable for output file
-#output_file = os.path.join("banking_results.txt")
 
 #  Open the output file
 #with open(output_file, "w") as datafile:
-    #writer = txt.writer(datafile)
-       # print(Election_df)
+ #   writer = txt.writer(datafile)
+  #     print(Election_df)
