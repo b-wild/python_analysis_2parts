76befa6c08c080bc91cdce54798bca80cec896a0
b-wild <bawilde79@gmail.com>
2023-01-15 20:47:19 -0500

    remove unneeded files

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,10 +7,24 @@ with open(Bank_df) as csvfile:
     csv_header=next(csvreader)
     print(f"CVS Header:{csv_header}")
     for row in csvreader:
-        print(row)
+        #print(row)
 # determing the number of months
-    Bank_df.len(0)
+    for months in range()
+        
+    print(f"There are (len(months)) months.")
+    
+    
 
+
+
+
+# Set variable for output file
+#output_file = os.path.join("banking_results.txt")
+
+#  Open the output file
+#with open(output_file, "w") as datafile:
+    #writer = txt.writer(datafile)
+       # print(Bank_df)
 
 
 
